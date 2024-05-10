4a87ed8583279210f1420a0b2efa29ca8385d1be
Alexey Shihalev <alexey.shihalev@internet.ru>
2024-05-10 10:17:30 +0300

    Соединен frontend с бэкендом, подключена база данных и добавление новых юзеров в БД при регистрации

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./itsite/main/static/js/header.jsx":
+/*!******************************************!*\
+  !*** ./itsite/main/static/js/header.jsx ***!
+  \******************************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (8:4)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n| const Header = () => {\\n|   return (\\n>     <div className=\\\"containerhead\\\">\\n|       <a href=\\\"https://electrotrans.spb.ru\\\">\\n|         <img src={logo} alt=\\\"\\\" className=\\\"img\\\" />\");\n\n//# sourceURL=webpack://pythonproject1/./itsite/main/static/js/header.jsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./itsite/main/static/js/header.jsx"]();
+/******/ 	
+/******/ })()
+;
